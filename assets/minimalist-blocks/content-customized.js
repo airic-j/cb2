77d6a8891b82b527cf57784532a0c343c64b5231
Eric Johnson <eric+github@ericslab.com>
2019-09-17 00:32:49 -0700

    undo snippet_path hiding

--- assets/minimalist-blocks/content-customized.js
+++ assets/minimalist-blocks/content-customized.js
@@ -7,16 +7,16 @@ function _tabs(n) {
     return '\n' + html;
 }
 
-// source: https: //stackoverflow.com/questions/2255689/how-to-get-the-file-path-of-the-currently-executing-javascript-code
-// function _path() {
-//     var scripts = document.querySelectorAll('script[src]');
-//     var currentScript = scripts[scripts.length - 1].src;
-//     var currentScriptChunks = currentScript.split('/');
-//     var currentScriptFile = currentScriptChunks[currentScriptChunks.length - 1];
-//     return currentScript.replace(currentScriptFile, '');
-// }
-//
-// var _snippets_path = _path();
+source: https: //stackoverflow.com/questions/2255689/how-to-get-the-file-path-of-the-currently-executing-javascript-code
+function _path() {
+    var scripts = document.querySelectorAll('script[src]');
+    var currentScript = scripts[scripts.length - 1].src;
+    var currentScriptChunks = currentScript.split('/');
+    var currentScriptFile = currentScriptChunks[currentScriptChunks.length - 1];
+    return currentScript.replace(currentScriptFile, '');
+}
+
+var _snippets_path = _path();
 
 
 var data_basic = {
